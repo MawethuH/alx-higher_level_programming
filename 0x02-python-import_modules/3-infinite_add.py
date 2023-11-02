fce5a7bfb9adee01713a8fd210139d4a63297445
MawethuH <hlophemawethu@gmail.com>
2023-11-02 14:49:00 -0700

    Import modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys, math
-    result = 0
-    for i in sys.argv:
-        result += int(i)
-        print("{}".format(result))
+import sys
+
+if __name__ != "__main__":
+    exit()
+
+i = 0
+result = 0
+for argument in sys.argv:
+    if i != 0:
+        result += int(argument)
+    else:
+        i += 1
+print("{:d}".format(result))
